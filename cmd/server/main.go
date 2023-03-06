314363faa43b203f5d468211eaa497d6650599f3
Maelys Tiffaney <maelysti@student.21-school.ru>
2023-03-06 23:31:54 +0300

    added interrupt

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,42 +1,32 @@
 package main
 
-import "github.com/mkrtychanr/wildberries_L0/internal/server"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
-// import (
-// 	"net/http"
-// 	"strconv"
+	"github.com/mkrtychanr/wildberries_L0/internal/server"
+)
 
-// 	"github.com/go-chi/chi/middleware"
-// 	"github.com/go-chi/chi/v5"
-// )
-
-// func handleHello(w http.ResponseWriter, r *http.Request) {
-// 	w.Write([]byte("Hello"))
-// }
-
-// func getID(w http.ResponseWriter, r *http.Request) {
-// 	id := chi.URLParam(r, "id")
-// 	_, err := strconv.Atoi(id)
-// 	if err != nil {
-// 		w.Write([]byte("Something went wrong"))
-// 		return
-// 	}
-// 	w.Write([]byte(id))
-// }
-
-// func main() {
-// 	r := chi.NewRouter()
-// 	r.Use(middleware.Logger)
-// 	r.Get("/", handleHello)
-// 	r.Get("/{id}", getID)
-// 	http.ListenAndServe(":1234", r)
-// }
+func HandleInterrupt(s *server.Server) {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Printf("\r")
+		s.Down()
+		os.Exit(0)
+	}()
+}
 
 func main() {
 	server, err := server.NewServer("config.json")
 	if err != nil {
 		panic(err)
 	}
+	HandleInterrupt(server)
 	server.Up()
-	defer server.Down()
+	for {
+	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,14 +1,24 @@
 package server
 
 import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/mkrtychanr/wildberries_L0/internal/database"
 	"github.com/mkrtychanr/wildberries_L0/internal/model"
+	"github.com/sirupsen/logrus"
 )
 
 type Server struct {
 	cache  map[string]model.Order
 	db     *database.Database
 	config *config
+	router *chi.Mux
 }
 
 func setConfigs(path string) (*database.Database, *config, error) {
@@ -32,13 +42,44 @@ func NewServer(path string) (*Server, error) {
 		db:     db,
 		cache:  make(map[string]model.Order),
 		config: config,
+		router: chi.NewRouter(),
 	}, nil
+}
+
+func (s *Server) startRouting() {
+	s.router.Use(middleware.Logger)
+	s.router.Get("/{id}", s.handleGetId)
+	address := fmt.Sprintf("%s%s", s.config.Host, s.config.Port)
+	str := fmt.Sprintf("Server is up on %s%s", s.config.Host, s.config.Port)
+	logrus.Info(str)
+	http.ListenAndServe(address, s.router)
 }
 
 func (s *Server) Up() {
 	s.db.Open()
+	logrus.Info("Database is up")
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Printf("\r")
+		s.Down()
+		os.Exit(0)
+	}()
+	go s.startRouting()
 }
 
 func (s *Server) Down() {
+	logrus.Info("Server is down")
 	s.db.Close()
+}
+
+func (s *Server) handleGetId(writer http.ResponseWriter, request *http.Request) {
+	id := chi.URLParam(request, "id")
+	_, ok := s.cache[id]
+	if !ok {
+		writer.Write([]byte("Something went wrong"))
+		return
+	}
+	writer.Write([]byte(id))
 }
