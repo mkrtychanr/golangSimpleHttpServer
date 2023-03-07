ec973f0761511e7994f746f60b46358f552baa9e
Maelys Tiffaney <maelysti@student.21-school.ru>
2023-03-07 21:24:18 +0300

    publisher

---
+++ cmd/publisher/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/nats-io/stan.go"
+)
+
+func main() {
+	sc, err := stan.Connect("test-cluster", "publisher", stan.NatsURL("nats://localhost:4222"))
+	if err != nil {
+		panic(err)
+	}
+	defer sc.Close()
+	path := ""
+	fmt.Printf("Enter the path to file: ")
+	fmt.Scan(path)
+	b, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	sc.Publish("addNewUser", b)
+}
